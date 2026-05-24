eb7cdfdaee0b30d50cd092b61258018f0269331e
Oscar Orozco <allmithy@gmail.com>
2026-05-24 12:07:48 -0600

    feat: add env.sample.php template for environment configuration

--- core/env.sample.php
+++ core/env.sample.php
@@ -7,5 +7,5 @@ define('DB_NAME', 'beautyandwell_prod');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
-define('BASE_URL', '/'); // En el host suele ser '/' si es el dominio principal
-define('GOOGLE_MAPS_API_KEY', 'AIzaSyAhJ3ApP1EPr_8IyZ8Unt-LlH1C8j5GZYE');
+define('BASE_URL', '/'); // En el host usa '/' si es el dominio principal o '/carpeta/'
+define('GOOGLE_MAPS_API_KEY', 'TU_API_KEY_AQUI');
